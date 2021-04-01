e8562b4c57874f74f34362a56c5d6d05635b616d
Linus Bergman <linusbergman@pm.me>
2021-04-01 21:43:01 +0300

    Fix refresh of index card when updating read info

--- scripts/main.js
+++ scripts/main.js
@@ -107,6 +107,28 @@ container.appendChild(inputFields);
 
 const status = (item) => myLibrary[item].read;
 
+function updateCard(id) {
+  const index = id.slice(4);
+  const card = document.getElementById(id);
+  const nextCard = document.getElementById(`Book${Number(index) + 1}`);
+  card.innerHTML = `<div class="cardImages"><h4>${
+    myLibrary[index].title
+  }</h4><input type="image" id="delButton${
+    card.id
+  }" src="../images/SeekPng.com_cross-png-transparent_601052.png" class="images"></div>
+	<p>by <em>${myLibrary[index].author}</em></p>
+<p>${myLibrary[index].pages} pages</p>
+	<div class="cardImages"><p>...${status(
+    index
+  )}</p><input type="image" id="checkButton${
+    card.id
+  }" src="../images/565-5650947_green-check-mark-clip-art.png" class="images" style="opacity: 1; align-self: flex-end;"></div>
+		`;
+  console.log(status(index));
+  cards.removeChild(card);
+  cards.insertBefore(card, nextCard);
+}
+
 for (item in myLibrary) {
   const book = document.createElement("div");
   book.className = "card";
@@ -135,19 +157,15 @@ for (item in myLibrary) {
   const checkButton = document.getElementById(`checkButton${book.id}`);
   checkButton.addEventListener("click", () => {
     const index = book.id.slice(4);
-    const card = document.getElementById(book.id);
-    const nextCard = document.getElementById(`Book${Number(index) + 1}`);
-    if (status(index) != "Read") {
+    if (myLibrary[index].read != "Read") {
       myLibrary[index].read = "Read";
+      updateCard(book.id);
       checkButton.style.opacity = "1";
-      console.log(myLibrary[index].read);
     } else {
       myLibrary[index].read = "not read yet";
+      updateCard(book.id);
       checkButton.style.opacity = "0.3";
-      console.log(myLibrary[index].read);
     }
-    cards.removeChild(card);
-    cards.insertBefore(card, nextCard);
   });
   if (myLibrary[item].read == "Read") {
     checkButton.style.opacity = "1";
